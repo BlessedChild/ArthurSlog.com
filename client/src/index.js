e6a8f028e414221946c04bb9340ad6b137156c83
BlessedChild <2250970846@qq.com>
2018-09-23 03:33:09 +0800

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import './background.jpg'
 import './style.css'
 
-document.getElementById("myheand").addEventListener('click', function(){document.getElementById("myheand").innerText = "V0.0.1"})    
+document.getElementById("myheand").addEventListener('click', function(){
+    document.getElementById("myheand").innerText = "V0.0.1"
+})    
